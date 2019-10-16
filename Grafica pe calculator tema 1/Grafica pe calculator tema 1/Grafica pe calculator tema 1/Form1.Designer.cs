2838882e397f5020424399a755844133e32e9def
Sergiu Manoliu <sermano14@gmail.com>
2019-10-16 21:02:15 +0300

    Tema 1

---
+++ Grafica pe calculator tema 1/Grafica pe calculator tema 1/Grafica pe calculator tema 1/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Grafica_pe_calculator_tema_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(915, 677);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(938, 117);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Value = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(944, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Thickness";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 679);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Grafica pe calculator tema 1/Grafica pe calculator tema 1/Grafica pe calculator tema 1/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grafica_pe_calculator_tema_1
+{
+    public partial class Form1 : Form
+    {
+        int x1, x2, y1, y2; // punctul a(x1,y1),b(x2,y2)
+        long tick = 0, tick1 = 0;
+        Bitmap map;
+        Timer aux; //Timer pentru limitarea randarii 
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            x2 = e.X;
+            y2 = e.Y;
+            MakeLine();
+        }
+
+        private void MakeLine()
+        {
+            map = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            BersenhamLine(x1,y1,x2,y2, trackBar1.Value);
+            pictureBox1.Image = map;
+        }
+
+        private void BersenhamLine(int x1, int y1, int x2, int y2, int thickness)
+        {
+            if (Math.Abs(y2 - y1) < Math.Abs(x2 - x1))
+            {
+                if (x1 > x2)
+                        plotLineHigh(x2, y2, x1, y1, thickness);
+                else
+                        plotLineHigh(x1, y1, x2, y2, thickness);
+                    
+            }
+            else
+            {
+                if (y1 > y2)
+                        plotLineLow(x2, y2, x1, y1, thickness);
+                else
+                        plotLineLow(x1, y1, x2, y2, thickness);
+                    
+            }
+        }
+
+        private void plotLineLow(int x1, int y1, int x2, int y2, int thickness)
+        {
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+            int xi = 1;
+            if (dx < 0)
+            {
+                xi = -1;
+                dx = -dx;
+            }
+            int D = 2 * dx - dy;
+            int x = x1;
+            for (int y = y1; y < y2; y++)
+            {
+                for (int i = -(thickness)/2; i <= (thickness) / 2; i++)
+                {
+
+                    for (int j = -(thickness) / 2; j <= (thickness) / 2; j++)
+                    {
+                        map.SetPixel(x + i, y + j, Color.Black);
+                    }
+                }
+                if (D > 0)
+                {
+                    x += xi;
+                    D -= 2 * dy;
+                }
+                D += 2 * dx;
+            }
+        }
+
+        private void plotLineHigh(int x1, int y1, int x2, int y2, int thickness)
+        {
+            int dx = x2 - x1;
+            int dy = y2 - y1;
+            int yi = 1;
+            if (dy < 0)
+            {
+                yi = -1;
+                dy = -dy;
+            }
+            int D = 2 * dy - dx;
+            int y = y1;
+            for (int x = x1; x < x2; x++)
+            {
+                for (int i = -(thickness) / 2; i <= (thickness) / 2; i++)
+                {
+
+                    for (int j = -(thickness) / 2; j <= (thickness) / 2; j++)
+                    {
+                        
+                        map.SetPixel(x + i, y + j, Color.Black);
+                        
+                    }
+                }
+                if (D > 0)
+                {
+                    y += yi;
+                    D -= 2 * dx;
+                }
+                D += 2 * dy;
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            aux.Start();
+            x1 = e.X;
+            y1 = e.Y;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                if (tick < tick1) 
+                {
+                    tick = tick1;
+                    x2 = e.X;
+                    y2 = e.Y;
+                    MakeLine();
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            map = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            aux = new Timer();
+            aux.Interval = 16;
+            aux.Tick += Aux_Tick;
+        }
+
+        private void Aux_Tick(object sender, EventArgs e)
+        {
+            tick1++;
+        }
+    }
+}
